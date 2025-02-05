a675f96f63114036f5b7e57b68836003fc804018
paulapreuss <paulavpreuss@outlook.com>
2025-02-05 14:40:45 +0100

    Add steps app

--- config/settings/base.py
+++ config/settings/base.py
@@ -95,6 +95,7 @@ LOCAL_APPS = [
     # Your stuff: custom apps go here
     "offgridplanner.projects",
     "offgridplanner.dashboard",
+    "offgridplanner.steps",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
