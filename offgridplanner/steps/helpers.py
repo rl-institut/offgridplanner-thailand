479115f7c577ac03f95a92b49fcd757f707f479b
paulapreuss <paulavpreuss@outlook.com>
2025-02-13 18:20:59 +0100

    Update and restructure helper functios

--- offgridplanner/steps/helpers.py
+++ offgridplanner/steps/helpers.py
@@ -1,106 +1,167 @@
-def get_demand_timeseries(nodes, custom_demand_parameters, all_profiles=None, df_only=True):
-    num_households = len(nodes[
-                             (nodes['consumer_type'] == 'household')
-                             & (nodes['is_connected'] == True)
-                         ].index)
-    calibration_target_value, calibration_option = get_calibration_target(custom_demand_parameters)
-    if all_profiles is None:
-        all_profiles = pd.read_parquet(path=config.FULL_PATH_PROFILES, engine="pyarrow")
-    df_hh_profile = combine_hh_profiles(all_profiles,
-                                        num_households=num_households,
-                                        demand_par_dict=custom_demand_parameters)
-    enterprise_nodes = nodes[(nodes['consumer_type'] == 'enterprise') & (nodes['is_connected'] == True)]
-    public_service_nodes = nodes[(nodes['consumer_type'] == 'public_service') & (nodes['is_connected'] == True)]
-    df_ent_profile = combine_ent_or_pubs_profiles(all_profiles, enterprise_nodes)
-    df_pub_profile = combine_ent_or_pubs_profiles(all_profiles, public_service_nodes)
-    df, calibration_factor = calibrate_profiles(df_hh_profile,
-                                                df_ent_profile,
-                                                df_pub_profile,
-                                                calibration_target_value,
-                                                calibration_option)
-    if df_only:
-        return df / 1000
+from datetime import timedelta
+
+import numpy as np
+import pandas as pd
+from django.forms import model_to_dict
+
+from config.settings.base import FULL_PATH_PROFILES
+
+LOAD_PROFILES = pd.read_parquet(path=FULL_PATH_PROFILES, engine="pyarrow")
+
+def is_ajax(request):
+    return request.headers.get("x-requested-with") == "XMLHttpRequest"
+
+def get_demand_timeseries(nodes, custom_demand, time_range=None):
+    """
+    Get the demand timeseries for the project
+    Parameters:
+        nodes (Nodes): Nodes object for the project
+        custom_demand (CustomDemand): CustomDemand object for the project
+        time_range (range): List of indices corresponding to timesteps (e.g. range(0,24) for first 24 hours)
+    Returns:
+        demand_df (pd.DataFrame): DataFrame with aggregated demands by columns "households", "enterprises", "public_services"
+    """
+    load_profiles = LOAD_PROFILES.iloc[time_range].copy() if time_range is not None else LOAD_PROFILES.copy()
+    # TODO change the index to pd.date_range(nodes.project.start_date, nodes.project.start_date + timedelta(nodes.project.n_days), freq='h'))
+    demand_df = pd.DataFrame(index=load_profiles.index)
+    demand_df["household"] = combine_profiles(nodes, "household", load_profiles, custom_demand=custom_demand)
+    demand_df["enterprise"] = combine_profiles(nodes, "enterprise", load_profiles)
+    demand_df["public_service"] = combine_profiles(nodes, "public_service", load_profiles)
+
+    demand_df = calibrate_profiles(demand_df, custom_demand)
+    return demand_df
+
+
+def calibrate_profiles(demand_df, custom_demand):
+    """
+    Calibrate demand profiles based on custom parameters.
+
+    Parameters:
+        demand_df (pd.DataFrame): DataFrame with three columns for household, enterprise and public_services demand
+        custom_demand (CustomDemand): CustomDemand instance for the project
+
+    Returns:
+        demand_df (pd.DataFrame): Calibrated demand based on peak or total annual demand
+    """
+    calibration_option = custom_demand.calibration_option
+    if calibration_option is None:
+        return demand_df
+
+    custom_demand_parameters = model_to_dict(custom_demand)
+    calibration_target = custom_demand_parameters[calibration_option]
+
+    if calibration_option == "annual_peak_consumption":
+        calibration_factor = calibration_target / demand_df.sum(axis=1).max()
+    elif calibration_option == "annual_total_consumption":
+        calibration_factor = calibration_target / demand_df.sum().sum()
     else:
-        return df / 1000, calibration_target_value, calibration_option, calibration_factor
+        raise ValueError(f"Unknown calibration option: {calibration_option}")
 
-def get_calibration_target(demand_par_dict):
-    if demand_par_dict['maximum_peak_load'] is not None:
-        value = float(demand_par_dict['maximum_peak_load'])
-        calibration_option = 'kW'
-    elif demand_par_dict['average_daily_energy'] is not None:
-        value = float(demand_par_dict['average_daily_energy'])
-        calibration_option = 'kWh'
+    return demand_df * calibration_factor
+
+
+def combine_profiles(nodes, consumer_type, load_profiles, custom_demand=None):
+    # TODO careful, logic will need fixing if name formatting in load profiles changes
+    """
+    Parameters:
+        nodes (Nodes): Nodes object
+        consumer_type (str): One of "household", "enterprise", "public_service"
+        load_profiles (pd.DataFrame): Load profiles
+        custom_demand (CustomDemand): CustomDemand object (only relevant for households)
+    Returns:
+        total_demand (pd.Series): Total demand for the given consumer type including machinery
+    """
+    node_counts = nodes.counts
+    consumer_type_counts = node_counts.loc[consumer_type]
+
+    # TODO crop load profiles depending on project.n_days and project.start_date
+    if consumer_type == "household":
+        custom_demand_parameters = model_to_dict(custom_demand)
+        total_demand = compute_household_demand(consumer_type_counts, custom_demand_parameters, load_profiles)
+
     else:
-        value = 1
-        calibration_option = None
-    return value, calibration_option
+        total_demand = compute_standard_demand(consumer_type, consumer_type_counts, load_profiles)
 
-def combine_ent_or_pubs_profiles(all_profiles, enterprises):
-    if enterprises is None or enterprises.empty:
-        return pd.DataFrame()
-    standard_ents = enterprises.query("consumer_type == 'enterprise'")
-    common_ent_profile = all_profiles[
-        "Enterprise_Large Load_Milling Machine"].copy()  # placeholder copy to keep same format before building up profile
-    common_ent_profile *= 0
-    if not standard_ents.empty:
-        for enterprise_index in standard_ents.index:
-            enterprise_type = standard_ents.loc[enterprise_index].consumer_detail.strip()
-            column_select_string = "Enterprise_" + enterprise_type
-            common_ent_profile += all_profiles[column_select_string]
-    public_services = enterprises.query("consumer_type == 'public_service'")
-    public_services_profile \
-        = all_profiles["Enterprise_Large Load_Milling Machine"].copy()  # placeholder copy to keep same format before building up profile
-    public_services_profile *= 0
-    if not public_services.empty:
-        for public_service_index in public_services.index:
-            public_service_type = public_services.loc[public_service_index].consumer_detail.strip()
-            column_select_string = "Public Service_" + public_service_type
-            public_services_profile += all_profiles[column_select_string]
-    large_load_ents = enterprises.query("(custom_specification.notnull()) & (consumer_type == 'enterprise')",
-                                        engine='python')
-    large_load_profile = all_profiles[
-        "Enterprise_Large Load_Milling Machine"].copy()  # placeholder copy to keep same format before building up profile
-    large_load_profile *= 0
-    if not large_load_ents.empty:
-        for enterprise_index in large_load_ents.index:
-            large_loads_list = large_load_ents.loc[enterprise_index].custom_specification.split(';')
-            # print("large_loads_list:", large_loads_list)
-            if large_loads_list[0] != '':
-                for load_type_and_count in large_loads_list:
-                    load_count = int(load_type_and_count.split("x")[0].strip())
-                    load_type = load_type_and_count.split("x")[1].split("(")[0].strip()
-                    enterprise_type = large_load_ents.loc[enterprise_index].consumer_detail.strip()
-                    column_select_string = "Enterprise_Large Load_" + load_type
-                    large_load_profile += (load_count * all_profiles[column_select_string])
-    total_non_household_profile = common_ent_profile + public_services_profile + large_load_profile
-    return total_non_household_profile
+    # Add machinery loads to enterprises
+    if consumer_type == "enterprise":
+        # Check if there are any large loads in the custom_specifications
+        if nodes.have_custom_machinery:
+            ent_nodes = nodes.filter_consumers("enterprise")
+            large_load_enterprises = ent_nodes[ent_nodes.custom_specification != ""]
+            machinery = unpack_machinery(large_load_enterprises)
+
+            # Compute machinery demand and add to enterprises
+            machinery_demand = compute_standard_demand("machinery", machinery, load_profiles)
+            total_demand += machinery_demand
+
+    return total_demand
 
 
-def combine_hh_profiles(all_profiles, num_households, demand_par_dict):
-    df_hh_profiles = \
-        all_profiles["Household_Distribution_Based_Very Low Consumption"] * float(demand_par_dict["custom_share_1"]) + \
-        all_profiles["Household_Distribution_Based_Low Consumption"] * float(demand_par_dict["custom_share_2"]) + \
-        all_profiles["Household_Distribution_Based_Middle Consumption"] * float(demand_par_dict["custom_share_3"]) + \
-        all_profiles["Household_Distribution_Based_High Consumption"] * float(demand_par_dict["custom_share_4"]) + \
-        all_profiles["Household_Distribution_Based_Very High Consumption"] * float(demand_par_dict["custom_share_5"])
-    df_hh_profiles *= num_households / 100
-    return df_hh_profiles
+def compute_household_demand(consumer_type_counts, custom_demand_params, load_profiles):
+    """
+    Compute demand for households applying wealth shares defined in CustomDemand.
+
+    Parameters:
+        consumer_type_counts (pd.DataFrame): Household nodes
+        custom_demand_params (dict): Custom demand shares dictionary
+        load_profiles (pd.DataFrame): Load profiles
+    Returns:
+        total_demand (pd.Series): Total household demand
+    """
+    total_demand = pd.Series(0, index=load_profiles.index)
+    total_households = consumer_type_counts.get("default", 0)
+
+    for demand_param, value in custom_demand_params.items():
+        if demand_param in ["very_low", "low", "middle","high", "very_high"]:
+            profile_col = f"Household_Distribution_Based_{demand_param.title().replace('_', ' ')} Consumption"
+            total_demand += load_profiles[profile_col] * value
+
+    return total_demand * total_households
 
 
-def calibrate_profiles(df_hh_profile, df_ent_profile, df_pub_profile, calibration_target_value, calibration_option=None):
-    calibration_factor = 1
-    df_lst = [df_hh_profile, df_ent_profile, df_pub_profile]
-    ts = [df for df in df_lst if not df.empty][0].index
-    for i, df in enumerate(df_lst):
-        if df.empty:
-            df_lst[i] = pd.DataFrame(0, index=ts, columns=['value'])
-    if calibration_option is not None:
-        if calibration_option == "kWh":
-            calibration_factor = calibration_target_value / ((df_hh_profile + df_ent_profile + df_pub_profile).sum() / 1000)
-        elif calibration_option == "kW":
-            calibration_factor = calibration_target_value / ((df_hh_profile + df_ent_profile + df_pub_profile).max() / 1000)
-        for i, df in enumerate(df_lst):
-            df_lst[i] = df_lst[i] * calibration_factor
-    df = pd.concat(df_lst, axis=1)
-    df.columns = ['households', 'enterprises', 'public_services']
-    return df, calibration_factor
+def compute_standard_demand(consumer_type, consumer_type_counts, load_profiles):
+    """
+    Compute demand for enterprises, public services or machinery.
+
+    Parameters:
+        consumer_type (str): One of "enterprise", "public_service" or "machinery"
+        consumer_type_counts (pd.DataFrame): Household nodes
+        load_profiles (pd.DataFrame): Load profiles
+    Returns:
+        total_demand (pd.Series): Total demand
+    """
+    if consumer_type == "machinery":
+        ts_string_prefix = f'Enterprise_Large Load'
+    else:
+        ts_string_prefix = f'{consumer_type.title().replace("_", " ")}'
+    ts_cols = [f'{ts_string_prefix}_{ts}' for ts in consumer_type_counts.index]
+    # import pdb; pdb.set_trace()
+    total_demand = load_profiles[ts_cols].dot(consumer_type_counts.values)
+
+    return total_demand
+
+
+def unpack_machinery(large_load_enterprises):
+    """
+    Unpack the machinery strings saved in custom_specification attribute of enterprise nodes.
+
+    Parameters:
+        large_load_enterprises (pd.DataFrame): Filtered nodes DataFrame by enterprises with custom machinery
+    Returns:
+        large_loads (pd.Series): Series with machinery as index and count column
+    """
+    # Split large loads string into list, then expand into separate rows
+    expanded = (
+        large_load_enterprises
+        .assign(custom_specification=lambda df: df['custom_specification'].str.split(';'))
+        .explode('custom_specification')
+        .reset_index(drop=True)
+    )
+    # Drop power ratings and extract counts from string, create series with machinery as index
+    large_loads = (
+        expanded["custom_specification"].str.extract(r'(\d+)\s*x\s*([^\(]+?)\s*(?:\(|$)')
+        .astype({0: int})
+        .groupby(1)[0].sum() # Sum duplicate machinery types
+    )
+
+    return large_loads
