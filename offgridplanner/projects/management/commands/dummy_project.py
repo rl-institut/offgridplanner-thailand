ca33b700e247590ef3875355195e243423f5e2e1
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-02-20 00:55:08 +0100

    create dummy user and project

--- offgridplanner/projects/management/commands/dummy_project.py
+++ offgridplanner/projects/management/commands/dummy_project.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from offgridplanner.projects.models import Project
-
+from offgridplanner.users.models import User
 
 class Command(BaseCommand):
     help = "Create a dummy a project "
@@ -9,6 +9,14 @@ class Command(BaseCommand):
     #     parser.add_argument("proj_id", nargs="+", type=int)
 
     def handle(self, *args, **options):
-        proj,_ = Project.objects.get_or_create(name="dummy", interest_rate=0.1)
+        user, _ = User.objects.get_or_create(
+            name="testUser",
+            email = "testUser@i.com",
+            password="pbkdf2_sha256$216000$1KxHDBWRSd4x$ieFKUFRElRR0rIRW0oDHy9/Mdw54k8tn0ifl5Xgu7ps=",
+        is_staff= False,
+        is_active= True,
+
+        )
+        proj,_ = Project.objects.get_or_create(name="dummy", interest_rate=0.1,user=user)
         print(proj.id)
         proj.save()
