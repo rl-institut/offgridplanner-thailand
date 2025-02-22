baeea860ec73a628eb94460f152211f3814d99ed
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-02-22 14:11:58 +0100

    Add url path to list projects and setup new projects

--- offgridplanner/projects/urls.py
+++ offgridplanner/projects/urls.py
@@ -5,7 +5,10 @@ from .views import *
 app_name = "projects"
 
 urlpatterns = [
+
+    path("", projects_list, name="home"),
     path("", projects_list, name="projects_list"),
+    path("<int:proj_id>", projects_list, name="projects_list"),
     path("duplicate/<int:proj_id>", project_duplicate, name="project_duplicate"),
     path("delete/<int:proj_id>", project_delete, name="project_delete"),
     path(
