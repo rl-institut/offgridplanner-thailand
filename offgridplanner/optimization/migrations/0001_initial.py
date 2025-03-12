9a7f6eb3cd1e470da112b0e1c3e1f05efd392807
paulapreuss <paulavpreuss@outlook.com>
2025-03-12 15:52:17 +0100

    Rename fresh migrations to initial

--- offgridplanner/optimization/migrations/0001_initial.py
+++ offgridplanner/optimization/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('projects', '0001_initial_fresh'),
+        ('projects', '0001_initial'),
     ]
 
     operations = [
