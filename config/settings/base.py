f8df485c9b18ba396a328bad0ce847d6aaa49680
paulapreuss <paulavpreuss@outlook.com>
2025-03-17 17:55:06 +0100

    Add metadata for form fields in csv file

--- config/settings/base.py
+++ config/settings/base.py
@@ -57,9 +57,9 @@ DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Demand data
-DEMAND_DIR = os.path.join(APPS_DIR, "static", "parquet")
-FULL_PATH_PROFILES = os.path.join(DEMAND_DIR, "1-hour_mean_365_days_all_users.parquet")
+# External data files
+DATA_DIR = os.path.join(APPS_DIR, "static", "data")
+FULL_PATH_PROFILES = os.path.join(DATA_DIR, "1-hour_mean_365_days_all_users.parquet")
 
 # URLS
 # ------------------------------------------------------------------------------
