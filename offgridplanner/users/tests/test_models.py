b2e99a59bce65f53adc2d58dc292a453d300bc22
paulapreuss <paulavpreuss@outlook.com>
2025-03-31 17:57:05 +0200

    Add language prefix to failing url tests

--- offgridplanner/users/tests/test_models.py
+++ offgridplanner/users/tests/test_models.py
@@ -1,5 +1,7 @@
+from django.utils.translation import get_language
+
 from offgridplanner.users.models import User
 
 
 def test_user_get_absolute_url(user: User):
-    assert user.get_absolute_url() == f"/users/{user.pk}/"
+    assert user.get_absolute_url() == f"/{get_language()}/users/{user.pk}/"
