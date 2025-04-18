4609d8b338c6bd284d7a5bf3db6a04f4546a31cb
paulapreuss <paulavpreuss@outlook.com>
2025-04-18 12:46:21 +0200

    Add field for each optimization instead of one
    To allow the tasks to run in parallel instead of one after the other

--- offgridplanner/optimization/models.py
+++ offgridplanner/optimization/models.py
@@ -74,7 +74,8 @@ class WeatherData(models.Model):
 
 class Simulation(models.Model):
     project = models.OneToOneField(Project, on_delete=models.CASCADE, null=True)
-    task_id = models.CharField(max_length=80, blank=True, default="")
+    task_grid = models.CharField(max_length=80, blank=True, default="")
+    task_supply = models.CharField(max_length=80, blank=True, default="")
     status = models.CharField(max_length=25, default="not yet started")
 
     def __str__(self):
