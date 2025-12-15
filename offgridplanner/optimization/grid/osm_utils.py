d4c90c04e2b8904b6a8916701d9ecda9e29fb77a
Celina.Kellinghaus <celina.kellinghaus@gmail.com>
2025-12-15 22:46:21 +0100

    Removed unused function

--- offgridplanner/optimization/grid/osm_utils.py
+++ offgridplanner/optimization/grid/osm_utils.py
@@ -262,38 +262,6 @@ def xy_coordinates_from_latitude_longitude(
     return x, y
 
 
-def fetch_roads_from_overpass(bbox):
-    """
-    bbox: (south, west, north, east)
-    Gets roads of any type from OSM.
-    """
-    query = f"""
-    [out:json][timeout:25];
-    (
-      way["highway"]({bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]});
-    );
-    out geom;
-    """
-
-    resp = requests.post(OVERPASS_URL, data={"data": query}, timeout=60)
-    resp.raise_for_status()
-    data = resp.json()
-
-    features = []
-    for el in data.get("elements", []):
-        if el.get("type") == "way" and "geometry" in el:
-            coords = [[pt["lon"], pt["lat"]] for pt in el["geometry"]]
-            features.append(
-                {
-                    "type": "Feature",
-                    "geometry": {"type": "LineString", "coordinates": coords},
-                    "properties": {"id": el.get("id")},
-                }
-            )
-
-    return {"type": "FeatureCollection", "features": features}
-
-
 def get_roads_within_boundaries(df):
     min_latitude, min_longitude, max_latitude, max_longitude = (
         df["latitude"].min(),
