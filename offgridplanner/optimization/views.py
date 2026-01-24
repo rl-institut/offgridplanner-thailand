bdbdbacd2fd322c5e3b2f7eece9ad54cf48e04b6
josihoppe <116898820+josihoppe@users.noreply.github.com>
2026-01-24 10:29:14 +0100

    fixed how to get number of households

--- offgridplanner/optimization/views.py
+++ offgridplanner/optimization/views.py
@@ -37,7 +37,6 @@ from offgridplanner.optimization.requests import optimization_server_request
 from offgridplanner.optimization.supply.demand_estimation import LOAD_PROFILES
 from offgridplanner.optimization.supply.demand_estimation import get_demand_timeseries
 from offgridplanner.optimization.tasks import revoke_task
-from offgridplanner.projects.helpers import collect_project_dataframes
 from offgridplanner.projects.helpers import df_to_file
 from offgridplanner.projects.models import Project
 from offgridplanner.steps.models import CustomDemand
@@ -503,15 +502,9 @@ def load_demand_plot_data(request, proj_id=None):
     timeseries["Average"] = timeseries["Average"].tolist()
 
     # need num_households to calculate demand for plots
-    dataframes = collect_project_dataframes(proj_id)
-    nodes_df = dataframes["nodes_df"]
-    num_households = len(
-        nodes_df[
-            (nodes_df["consumer_type"] == "household")
-            & (nodes_df["is_connected"] == True)  # noqa:E712
-        ],
-    )
+    num_households = len(nodes.filter_consumers("household"))
     timeseries["num_households"] = num_households
+
     return JsonResponse({"timeseries": timeseries})
 
 
