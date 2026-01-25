fadd32c6c1215ed563e73ae6a949442f838bc5db
Celina.Kellinghaus <celina.kellinghaus@gmail.com>
2026-01-25 18:33:14 +0100

    Add is_clicked column when saving roads

--- offgridplanner/optimization/views.py
+++ offgridplanner/optimization/views.py
@@ -411,10 +411,17 @@ def roads_to_db(request, proj_id=None):
             return JsonResponse({"message": "No valid data"}, status=200)
 
         df = df.drop_duplicates(subset=["road_id"], keep="first")
-        required_columns = ["road_id", "coordinates", "how_added", "road_type"]
+        required_columns = [
+            "road_id",
+            "coordinates",
+            "how_added",
+            "road_type",
+            "is_clicked",
+        ]
         df = df[required_columns]
         df["how_added"] = df["how_added"].fillna("automatic")
         df["road_type"] = df["road_type"].fillna("osm")
+        df["is_clicked"] = df["is_clicked"].fillna(value=False)
 
         roads, _ = Roads.objects.get_or_create(project=project)
 
