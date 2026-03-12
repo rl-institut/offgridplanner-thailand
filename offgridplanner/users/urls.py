2591492ae34cfe933e655c40c9c2f49345755633
paulapreuss <paulavpreuss@outlook.com>
2026-03-12 17:26:06 +0100

    Include demo user function in login view

--- offgridplanner/users/urls.py
+++ offgridplanner/users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
+    path("accounts/login/", view=login_view, name="login"),
     path(
         "demo_convert_account", view=demo_convert_account, name="demo_convert_account"
     ),
