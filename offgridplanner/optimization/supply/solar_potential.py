1dfa34a22b94872c0b860ad66bd7de95772cfeeb
paulapreuss <paulavpreuss@outlook.com>
2026-05-19 11:47:43 +0200

    Delete unused code

--- offgridplanner/optimization/supply/solar_potential.py
+++ offgridplanner/optimization/supply/solar_potential.py
@@ -9,10 +9,7 @@ combined with detailed solar panel and inverter specifications, enables it to ca
 """
 
 import logging
-import warnings
-from pathlib import Path
 
-import geopandas as gpd
 import numpy as np
 import pandas as pd
 import pvlib
@@ -22,83 +19,10 @@ from pvlib.modelchain import ModelChain
 from pvlib.pvsystem import PVSystem
 from pvlib.temperature import TEMPERATURE_MODEL_PARAMETERS
 
-from config.settings.base import CDS_API_KEY
 from offgridplanner.optimization.requests import request_renewables_ninja_pv_output
 from offgridplanner.optimization.requests import request_weather_data
 
 logger = logging.getLogger(__name__)
-
-# TODO this will no longer be needed, for local try to mode from oginal ogp mySQL
-# originally in sync_queries.py
-# def update_weather_db(country='Nigeria', year=None):
-#     if year is not None and year >= (pd.Timestamp.now() + pd.Timedelta(24 * 7, unit='H')).year:
-#         raise Exception("This function excepts available weather data for a entire year, "
-#                         "but for {} that data is not yet available".format(year))
-#     elif year != 2022:
-#         warnings.warn("Currently, only simulation the year 2022 is possible. Refer to the comments for "
-#                       "detailed explanations.")
-# year = (pd.Timestamp.now() + pd.Timedelta(24 * 14, unit='H')).year - 1 if year is None else int(year)
-# year = 2022 # so fast demand data is only available for 2022 and start_date is always 2022-01-01 and max. duration
-# # is one year (see func 'save_project_setup' in static/js/backend_communications.js )
-# for month in range(1, 13, 3):  # Increment by 3
-#     start_date = pd.Timestamp(year=year, month=month, day=1) - pd.Timedelta(25, unit='H')
-#     end_month = month + 2  # Third month in the interval
-#     end_month = 12 if end_month > 12 else end_month  # Ensure it does not exceed December
-#     last_day_of_end_month = calendar.monthrange(year, end_month)[1]
-#     end_date = pd.Timestamp(year=year, month=end_month, day=last_day_of_end_month) + pd.Timedelta(25, unit='H')
-#     file_name = 'cfd_weather_data_{}.nc'.format(start_date.strftime('%Y-%m'))
-#     data_xr = download_weather_data(start_date, end_date, country=country, target_file=file_name).copy()
-#     df = prepare_weather_data(data_xr)
-#     df.to_csv("weather_data.csv")
-# insert_df(WeatherData, df)
-
-
-# def insert_df(model_class, df, user_id=None, project_id=None):
-#     if user_id is not None and project_id is not None:
-#         user_id, project_id = int(user_id), int(project_id)
-#     df = df.dropna(how='all', axis=0)
-#     if not df.empty:
-#         if user_id is not None and project_id is not None:
-#             remove(model_class, user_id, project_id)
-#             df['id'] = int(user_id)
-#             df['project_id'] = int(project_id)
-#         if hasattr(model_class, 'dt') and 'dt' not in df.columns:
-#             df.index.name = 'dt'
-#             df = df.reset_index()
-#         _insert_df(model_class.__name__.lower(), df, if_exists='update')
-
-
-def create_cdsapirc_file():
-    home_dir = Path("~").expanduser()
-    file_path = Path(home_dir) / ".cdsapirc"
-    if Path(file_path).exists:
-        print(f".cdsapirc file already exists at {file_path}")
-        return
-    content = f"url: https://cds.climate.copernicus.eu/api/v2\nkey: {CDS_API_KEY}"
-    with Path(file_path).open("w") as file:
-        file.write(content)
-    print(f".cdsapirc file created at {file_path}")
-
-
-def download_weather_data(start_date, end_date, country="Nigeria", target_file="file"):
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-    country_shape = world[world["name"] == country]
-    geopoints = country_shape.geometry.iloc[0].bounds
-    lat = [geopoints[0], geopoints[2]]
-    lon = [geopoints[1], geopoints[3]]
-    variable = "pvlib"
-    create_cdsapirc_file()
-    data_xr = era5.get_era5_data_from_datespan_and_position(
-        variable=variable,
-        start_date=start_date.strftime("%Y-%m-%d"),
-        end_date=end_date.strftime("%Y-%m-%d"),
-        latitude=lat,
-        longitude=lon,
-        target_file=target_file,
-    )
-    return data_xr
 
 
 def prepare_weather_data(data_xr):
